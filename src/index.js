50d6d51e104ce7c51575b7fe5e87579cc5c18b1a
aigerimr <raisovaaygerim@mail.ru>
2022-02-04 17:01:44 +0600

    feat: add code to solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    nString = n.toString();
+    let y = "";
+    for (let i = nString.length - 1; i >= 0; i--) {
+        y = y + nString[i];
+    }
+    // console.log(+y);
+    return +y;
+};
